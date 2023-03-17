b0f6f5f9d33f43ce485b0c23bc573407891082b5
Yulia Zherebtsova <juliaz1206@yandex.ru>
2023-03-17 04:33:27 +0300

    feat: weather service (#6)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
 
-export class AppComponent {
-  title = 'angular-weather-app';
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Moscow').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  openWeatherApiKey: '02f0008cfdc2db1da28d3ca36700c108'
 };
