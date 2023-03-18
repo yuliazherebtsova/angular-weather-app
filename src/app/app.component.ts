d3182d912d86c858fbbc322f20a4d9b2a719379c
Yulia Zherebtsova <juliaz1206@yandex.ru>
2023-03-18 04:33:24 +0300

    feat: data binding (#12)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
 
+enum Temperature {
+  VeryCold = -18,
+  Cold = -9,
+  Cool = 6,
+  Warm = 15,
+  Hot = 20,
+  VeryHot = 27,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  weatherData$: Observable<WeatherData>;
+  temperature = Temperature;
+
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Moscow').subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
+    this.loadWeather();
+  }
+
+  private loadWeather(): void {
+    this.weatherData$ = this.weatherService.getWeatherData('Moscow');
   }
 }
