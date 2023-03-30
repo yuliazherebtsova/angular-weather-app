c933f724d3bdc4b7d0ab1720be3ecc70635dbe70
Yulia Zherebtsova <juliaz1206@yandex.ru>
2023-03-31 01:55:58 +0300

    feat: city autocomplete (#24)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { TitleCasePipe } from './pipes/title-case.pipe';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleCasePipe
   ],
   imports: [
     BrowserModule,
